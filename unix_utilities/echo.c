504688aa7b66a36140414239acbca933ca1acdca
abdo <abdo@debian.eng.abdo>
2026-08-14 14:11:16 -0400

    Edit file

--- unix_utilities/echo.c
+++ unix_utilities/echo.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, int *argv[]){
-	for(int i = 0; i < argc; i++){
-		printf(argv[i + 1]);	
+	for(int i = 1; i < argc; i++){
+		printf("%s", argv[i]);
+		if ((i + 1) < argc){
+			printf(" ");
+		}
 	}
 	printf("\n");
 	return 0;
